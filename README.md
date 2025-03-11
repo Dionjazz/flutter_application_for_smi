7ac493ce65e34cfa211bd847c1548ee75fc65781
Денис <sapaev@temp.niitp>
2025-03-12 01:50:58 +0300

    deploy

--- README.md
+++
@@ -1,3 +0,0 @@
-# flutter_application_for_smi
-
-Flutter web app with HTML image
--- android/app/src/main/kotlin/com/example/flutter_application_for_smi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_application_for_smi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
